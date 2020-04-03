d563694e87c4aba6a4c5da13fa333dcf3f8c0080
Ashley M Mader <ashley.mader12@gmail.com>
2020-04-03 15:17:14 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,15 +5,16 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MAX(gpa) FROM students;"
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students;"
 end
